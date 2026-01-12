e7bc31b010f4787b28bcbdb8fd88b78370318c38
Martynas Povilaitis <martynas.povilaitis@workofo.com>
2026-01-12 10:30:21 +0200

    Remove package

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,7 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { WorkerEntrypoint } from 'cloudflare:workers';
 import { appRouter } from './router';
-import { instrument, ResolveConfigFn } from '@microlabs/otel-cf-workers';
-
-const config: ResolveConfigFn = (env: {}, _trigger) => {
-	return {
-    exporter: {} as any,
-		service: { name: 'greetings', spanAttributeMappings: { 'http.route': 'cf.worker.route' } },
-	}
-}
-
 
 const handler = {
   async fetch(request: Request): Promise<Response> {
@@ -32,5 +22,5 @@ const handler = {
   }
 }
 
-export default instrument(handler, config)
+export default handler;
 
